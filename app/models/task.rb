2024f1126a239b5c2469b18571cee6e85f6595b2
edouard modolo <edouardtravers@gmail.com>
2024-12-13 18:14:39 +0100

    MAJ OK

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,12 @@
 class Task < ApplicationRecord
+
+  before_save :capitalize_title
+
+  private
+
+  # Méthode qui capitalise le titre avant la sauvegarde
+  def capitalize_title
+    self.title = title.capitalize
+  end
+
 end
